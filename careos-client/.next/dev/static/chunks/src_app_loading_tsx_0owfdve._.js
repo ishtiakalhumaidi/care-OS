9b29b69dd4d4eeeb68af8eabdcf8b44892a8115c
Ishtiak Al Humaidi <ishtiakalhumaidi@gmail.com>
2026-07-23 16:29:13 +0600

    Remove orphaned .next build cache, rename care-os to careos-client

--- careos-client/.next/dev/static/chunks/src_app_loading_tsx_0owfdve._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0yf-_lucide-react_dist_esm_1fbjml8._.js"],
-    source: "dynamic"
-});
